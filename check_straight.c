e981f0247a67a47189cf3ef9871f338ef7da3ef2
marc <marc@mkali.domain.local>
2021-05-29 17:20:35 +0200

    .

--- check_straight.c
+++ check_straight.c
@@ -1,16 +1,21 @@
+
+#include "stdio.h"
 #include <stdbool.h>
 
 bool checkStraightLine(int **coordinates, int coordinatesSize, int* coordinatesColSize)
-{
+{   
     int i = 1;
-    int diferent[] = {(coordinates[1][0] - coordinates[0][0])  , (coordinates[1][1] - coordinates[0][1])};
+    int *diferent = malloc(sizeof(int ) * 2);
+    diferent[0] = (coordinates[1][0] - coordinates[0][0]); 
+    diferent[1] = (coordinates[1][1] - coordinates[0][1]);
 
-    while (i != coordinatesSize) {
-        if (((coordinates[i][0] - coordinates[i - 1][0])) != diferent[0])
+    while (i < coordinatesSize) {
+        if (((coordinates[i][0] - coordinates[i - 1][0])) != diferent[0]){
             return (false);
-        if ((coordinates[i][1] - coordinates[i][1]) != diferent[1])
+        }
+        if ((coordinates[i][1] - coordinates[i - 1][1]) != diferent[1])
             return (false);
         i++;
     }
     return (true);
-}	
+}
